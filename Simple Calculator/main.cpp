c8ab72a99f60cbb339fbd60a6f684a51a1e15f08
Erkinov Amirbek <erkinovamir100@gmail.com>
2024-09-22 23:45:23 +0500

    Finished: all done

--- Simple Calculator/main.cpp
+++ Simple Calculator/main.cpp
@@ -6,9 +6,73 @@
 //
 
 #include <iostream>
+#include <cstring>
+#include <string>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+
+using namespace std;
+
+int add(int x,int y){
+    return x + y;
+}
+
+int multiply(int x,int y){
+    return x * y;
+}
+
+int min(int x,int y){
+    return x - y;
+}
+
+int devide(int x,int y){
+    return x / y;
+}
+
+void answer(int result){
+    cout << "your result is: " << result << endl;
+    cout << "________________________" << endl;
+}
+
+int main() {
+    cout << "Welcome to the calculator app!" << endl;
+    
+    while (true) {
+        char oper;
+        int x;
+        int y;
+        int result;
+        
+        
+        
+        cout << "enter your operation: ";
+        cin >> oper;
+        
+        cout << "Enter first number: ";
+        cin >> x;
+        
+        cout << "Enter second number: " ;
+        cin >> y;
+        
+        switch (oper) {
+            case '+':
+                result = add(x, y);
+                answer(result);
+                break;
+            case '-':
+                result = min(x, y);
+                answer(result);
+                break;
+            case '*':
+                result = multiply(x, y);
+                answer(result);
+                break;
+            case '/':
+                result = devide(x, y);
+                answer(result);
+                break;
+        }
+        
+        
+    }
     return 0;
 }
